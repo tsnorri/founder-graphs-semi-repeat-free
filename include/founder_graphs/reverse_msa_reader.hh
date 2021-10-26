f26b81886e57f29b57d3f7748e7de7078f41ba10
Tuukka Norri <tsnorri@iki.fi>
2021-10-26 20:02:28 +0300

    Add MSA reader for bgzip files
    – The reader takes bgzip files as input and uses libdispatch to decompress the blocks in parallel.

--- include/founder_graphs/reverse_msa_reader.hh
+++ include/founder_graphs/reverse_msa_reader.hh
@@ -6,6 +6,9 @@
 #ifndef FOUNDER_GRAPHS_REVERSE_MSA_READER_HH
 #define FOUNDER_GRAPHS_REVERSE_MSA_READER_HH
 
+#include <founder_graphs/bgzip_reader.hh>
+#include <functional>
+#include <libbio/dispatch/dispatch_ptr.hh>
 #include <libbio/file_handle.hh>
 #include <string>
 #include <vector>
@@ -13,26 +16,63 @@
 
 namespace founder_graphs {
 	
+	// TODO: Make this set of classes more general-purpose by e.g. by allowing traversal from the beginning or random access.
 	class reverse_msa_reader
 	{
 	public:
-		typedef std::vector <char>		buffer_type;
-	
+		typedef std::vector <char>			buffer_type;
+		typedef std::function <bool(bool)>	fill_buffer_callback_type;
+		
 	protected:
-		std::vector <libbio::file_handle>	m_handles;
 		buffer_type							m_buffer;
 		std::size_t							m_file_position{};
-		std::size_t							m_aln_size{};
-		std::size_t							m_preferred_block_size{}; // Hopefully the same for all handles.
-	
+		std::size_t							m_current_block_size{};
+		
 	public:
-		void add_file(std::string const &path);
-		void prepare();
-		bool fill_buffer();
-		std::size_t aligned_size() const { return m_aln_size; }
-		std::size_t block_size() const { return m_preferred_block_size; }
-		std::size_t handle_count() const { return m_handles.size(); }
+		virtual void add_file(std::string const &path) = 0;
+		virtual void prepare() = 0;
+		virtual bool fill_buffer(fill_buffer_callback_type &) = 0;
+		bool fill_buffer(fill_buffer_callback_type &&cb) { return fill_buffer(cb); }
+		
 		buffer_type const &buffer() const { return m_buffer; }
+		std::size_t block_size() const { return m_current_block_size; }
+		virtual std::size_t aligned_size() const = 0;
+		virtual std::size_t handle_count() const = 0;
+	};
+	
+	
+	class text_reverse_msa_reader final : public reverse_msa_reader
+	{
+	protected:
+		std::vector <libbio::file_handle>	m_handles;
+		std::size_t							m_preferred_block_size{}; // Hopefully the same for all handles.
+		std::size_t							m_aligned_size{};
+		
+	public:
+		void add_file(std::string const &path) override;
+		void prepare() override;
+		using reverse_msa_reader::fill_buffer;
+		bool fill_buffer(fill_buffer_callback_type &) override;
+		
+		std::size_t aligned_size() const override { return m_aligned_size; }
+		std::size_t handle_count() const override { return m_handles.size(); }
+	};
+	
+	
+	class bgzip_reverse_msa_reader final : public reverse_msa_reader
+	{
+	protected:
+		std::vector <bgzip_reader>				m_handles;
+		libbio::dispatch_ptr <dispatch_group_t>	m_decompress_group;
+		
+	public:
+		void add_file(std::string const &path) override;
+		void prepare() override;
+		using reverse_msa_reader::fill_buffer;
+		bool fill_buffer(fill_buffer_callback_type &) override;
+		
+		std::size_t aligned_size() const override { return (m_handles.empty() ? 0 : m_handles.front().uncompressed_size()); }
+		std::size_t handle_count() const override { return m_handles.size(); }
 	};
 }
 
--- libfoundergraphs/reverse_msa_reader.cc
+++ libfoundergraphs/reverse_msa_reader.cc
@@ -5,89 +5,152 @@
 
 #include <cstdio> // strerror
 #include <founder_graphs/reverse_msa_reader.hh>
+#include <founder_graphs/utility.hh>
 #include <libbio/assert.hh>
+#include <libbio/dispatch/dispatch_fn.hh>
 #include <libbio/file_handling.hh>
 #include <range/v3/view/enumerate.hpp>
-#include <stdexcept> // std::runtime_error
-#include <sys/stat.h> // fstat
-#include <sys/types.h> // pread
-#include <sys/uio.h> // pread
 #include <tuple>
-#include <unistd.h> // pread
 
 namespace lb	= libbio;
 namespace rsv	= ranges::views;
 
 
-namespace {
-	
-	std::tuple <std::size_t, std::size_t> check_sizes(lb::file_handle const &handle)
-	{
-		auto const fd(handle.get());
-		struct stat sb{};
-		
-		if (-1 == fstat(fd, &sb))
-			throw std::runtime_error(strerror(errno));
-		
-		if (sb.st_size < 0)
-			return {0, 0};
-		
-		if (sb.st_blksize < 0) // Unsigned on both macOS and Linux but test anyway.
-			return {0, 0};
-		
-		return {sb.st_size, sb.st_blksize};
-	}
-	
-	
-	void read_from_file(lb::file_handle const &handle, std::size_t const pos, std::size_t const read_count, char *buffer_start)
-	{
-		auto const res(::pread(handle.get(), buffer_start, read_count, pos));
-		if (-1 == res)
-			throw std::runtime_error(strerror(errno));
-		
-		libbio_always_assert_eq(read_count, res);
-	}
-}
-
-
 namespace founder_graphs {
 	
-	void reverse_msa_reader::add_file(std::string const &path)
+	void text_reverse_msa_reader::add_file(std::string const &path)
 	{
 		bool const is_empty(m_handles.empty());
 		auto const &handle(m_handles.emplace_back(lb::open_file_for_reading(path)));
 		
-		auto const [aln_size, preferred_block_size] = check_sizes(handle);
+		auto const [aligned_size, preferred_block_size] = check_file_size(handle);
 		if (is_empty)
 		{
-			m_aln_size = aln_size;
+			m_aligned_size = aligned_size;
 			m_preferred_block_size = preferred_block_size;
 		}
 		else
 		{
-			libbio_always_assert_eq(m_aln_size, aln_size);
+			libbio_always_assert_eq(m_aligned_size, aligned_size);
 			libbio_always_assert_eq(m_preferred_block_size, preferred_block_size);
 		}
 	}
 	
 	
-	void reverse_msa_reader::prepare()
+	void text_reverse_msa_reader::prepare()
 	{
 		m_buffer.resize(m_handles.size() * m_preferred_block_size, 0);
-		m_file_position = m_aln_size;
+		m_file_position = m_aligned_size;
 	}
 	
 	
-	bool reverse_msa_reader::fill_buffer()
+	bool text_reverse_msa_reader::fill_buffer(fill_buffer_callback_type &cb)
 	{
 		if (0 == m_file_position)
+		{
+			cb(false);
 			return false;
+		}
 		
-		auto const read_count(std::min(m_file_position, m_preferred_block_size));
-		m_file_position -= read_count;
+		m_current_block_size = std::min(m_file_position, m_preferred_block_size);
+		m_file_position -= m_current_block_size;
 		for (auto const &[i, handle] : rsv::enumerate(m_handles))
-			read_from_file(handle, m_file_position, read_count, m_buffer.data() + i * m_preferred_block_size);
+			read_from_file(handle, m_file_position, m_current_block_size, m_buffer.data() + i * m_current_block_size);
 		
-		return true;
+		return cb(true);
+	}
+	
+	
+	void bgzip_reverse_msa_reader::add_file(std::string const &path)
+	{
+		auto &handle(m_handles.emplace_back());
+		handle.open(path);
+	}
+	
+	
+	void bgzip_reverse_msa_reader::prepare()
+	{
+		if (m_handles.empty())
+			return;
+		
+		// Prepare the decompression group.
+		m_decompress_group.reset(dispatch_group_create());
+		
+		// Check for matching index entries.
+		auto const &first_handle(m_handles.front());
+		auto const &first_entries(first_handle.index_entries());
+		auto const first_count(first_handle.block_count());
+		for (std::size_t i(1); i < m_handles.size(); ++i)
+			libbio_always_assert_eq(m_handles[i].block_count(), first_count);
+		
+		for (std::size_t i(0); i < first_count; ++i)
+		{
+			for (std::size_t j(1); j < m_handles.size(); ++j)
+			{
+				auto const &entries(m_handles[j].index_entries());
+				libbio_always_assert_eq(first_entries[i].uncompressed_offset, entries[i].uncompressed_offset);
+			}
+		}
+		
+		// Determine the max. block size.
+		std::size_t max_uncompressed_block_size{};
+		for (std::size_t i(1); i < first_count; ++i)
+		{
+			auto const prev_offset(first_entries[i - 1].uncompressed_offset);
+			auto const offset(first_entries[i].uncompressed_offset);
+			max_uncompressed_block_size = std::max(max_uncompressed_block_size, offset - prev_offset);
+		}
+		
+		// Reserve memory.
+		m_buffer.resize(m_handles.size() * max_uncompressed_block_size, 0);
+		
+		// Move past the last block.
+		for (std::size_t i(0); i < m_handles.size(); ++i)
+		{
+			auto &handle(m_handles[i]);
+			handle.block_seek(first_count);
+		}
+	}
+	
+	
+	bool bgzip_reverse_msa_reader::fill_buffer(fill_buffer_callback_type &cb)
+	{
+		if (0 == m_handles.front().current_block())
+		{
+			cb(false);
+			return false;
+		}
+		
+		// Seek.
+		for (auto &handle : m_handles)
+		{
+			bool const status(handle.block_seek_previous());
+			libbio_assert(status);
+		}
+		
+		// Update the uncompressed size.
+		auto const &first_handle(m_handles.front());
+		m_current_block_size = first_handle.current_block_uncompressed_size();
+		m_buffer.resize(m_handles.size() * m_current_block_size);
+		
+		// Fill the buffers and decompress.
+		{
+			auto const queue(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0));
+			for (std::size_t i(0); i < m_handles.size(); ++i)
+			{
+				auto &handle(m_handles[i]);
+				handle.read_current_block();
+				lb::dispatch_group_async_fn(*m_decompress_group, queue, [this, i, &handle](){
+					auto const it(m_buffer.data() + i * m_current_block_size);
+					auto const end(it + m_current_block_size);
+					handle.decompress(std::span(it, end));
+				});
+			}
+			
+			if (0 == dispatch_group_wait(*m_decompress_group, DISPATCH_TIME_FOREVER))
+				return cb(true);
+			else
+				throw std::runtime_error("dispatch_group_wait failed even though DISPATCH_TIME_FOREVER was specified as timeout.");
+		}
 	}
 }
