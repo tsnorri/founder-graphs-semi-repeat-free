879c2b1e717dbdebf00ab73bb29ab37fa8315860
Tuukka Norri <tsnorri@iki.fi>
2021-10-28 18:18:38 +0300

    Handle precalculated data structures

--- build_cst/main.cc
+++ build_cst/main.cc
@@ -12,10 +12,26 @@
 
 namespace {
 	
-	void build_cst(char const *input_path)
+	void build_cst(
+		char const *input_path,
+		char const *text_path,
+		char const *sa_path,
+		char const *bwt_path,
+		char const *csa_path
+	)
 	{
+		sdsl::cache_config config(false); // Do not remove temporary files automatically.
+		if (text_path)
+			config.file_map[sdsl::conf::KEY_TEXT] = text_path;
+		if (sa_path)
+			config.file_map[sdsl::conf::KEY_SA] = sa_path;
+		if (bwt_path)
+			config.file_map[sdsl::conf::KEY_BWT] = bwt_path;
+		if (csa_path)
+			config.file_map[sdsl::conf::KEY_CSA] = csa_path;
+
 		founder_graphs::cst_type cst;
-		sdsl::construct(cst, input_path, 1);
+		sdsl::construct(cst, input_path, config, 1);
 		
 		// Output.
 		cereal::PortableBinaryOutputArchive archive(std::cout);
@@ -38,7 +54,13 @@ int main(int argc, char **argv)
 	std::ios_base::sync_with_stdio(false);	// Don't use C style IO after calling cmdline_parser.
 	std::cin.tie(nullptr);					// We don't require any input from the user.
 	
-	build_cst(args_info.input_arg);
+	build_cst(
+		args_info.input_arg,
+		args_info.text_arg,
+		args_info.sa_arg,
+		args_info.bwt_arg,
+		args_info.csa_arg
+	);
 	
 	return EXIT_SUCCESS;
 }
