e3371d9aef35ca1884bd22e3562e19bc47bcafc4
tsnorri <tsnorri@iki.fi>
2021-11-11 16:17:37 +0200

    Rewrite optimization algorithm
    – Implemented the first two optimization options in the paper, namely maximizing block count and minimizing the maximum segment length.
    – pBWT is only (possibly) needed for optimizing the number of segments or similar, so I discarded it for now.

--- optimize_segmentation/main.cc
+++ optimize_segmentation/main.cc
@@ -5,184 +5,236 @@
 
 
 #include <cereal/archives/portable_binary.hpp>
-#include <founder-graphs/msa_index.hh>
-#include <iostream>
+#include <founder_graphs/basic_types.hh>
 #include <libbio/assert.hh>
 #include <libbio/file_handling.hh>
+#include <range/v3/view/reverse.hpp>
 #include <set>
 #include <vector>
 #include "cmdline.h"
 
+namespace fg	= founder_graphs;
 namespace lb	= libbio;
+namespace rsv	= ranges::views;
 
 
 namespace {
 	
-	struct dp_state
+	typedef std::uint64_t score_type;
+	
+	
+	// Interval without a score.
+	struct interval
 	{
-		std::size_t score{SIZE_MAX};
-		std::size_t pos{SIZE_MAX};
+		fg::length_type lb{};
+		fg::length_type rb{};
 		
-		dp_state() = default;
+		interval() = default;
 		
-		dp_state(std::size_t const score_, std::size_t const pos_):
-			score(score_),
-			pos(pos_)
+		interval(fg::length_type const lb_, fg::length_type const rb_):
+			lb(lb_),
+			rb(rb_)
 		{
+		}
+		
+		bool is_valid() const { return fg::LENGTH_MAX != rb; }
+		fg::length_type length() const { return rb - lb; }
+	};
+	
+	struct interval_rb_cmp
+	{
+		bool operator()(interval const &lhs, interval const &rhs) const
+		{
+			return lhs.rb < rhs.rb;
 		}
 	};
 	
 	
-	void optimize_segmentation(char const *sequence_list_path)
+	// Interval with a score and a pointer to the next interval.
+	struct scored_interval
 	{
-		lb::open_file_for_reading(sequence_list_path, sequence_path_stream);
+		scored_interval	const	*next{};		// Due to how this struct is used, next needs to be const *.
+		interval				location{};
+		score_type				score{};
 		
-		reverse_msa_reader reader;
+		scored_interval() = default;
+		
+		explicit scored_interval(interval const &location_):
+			location(location_)
 		{
-			std::string line;
-			while (std::getline(sequence_path_stream, line))
-				reader.add_file(line);
 		}
 		
-		// Prepare the reader.
-		reader.prepare();
-		auto const seq_count(reader.handle_count());
-		auto const block_size(reader.block_size());
-		auto const aligned_size(reader.aligned_size());
-		
-		if (0 == seq_count)
-			return;
-		
-		// Handle the segment boundaries.
-		cereal::PortableBinaryInputArchive iarchive(std::cin);
-		
+		scored_interval(fg::length_type const lb_, fg::length_type const rb_):
+			location(lb_, rb_)
 		{
-			std::size_t input_count{};
-			iarchive(cereal::make_size_tag(input_count));
-			libbio_always_assert_eq(aligned_size, input_count);
 		}
 		
-		// Reserve seq_count space for everything in order to avoid reallocations.
-		std::vector <std::size_t> src_seq_indices(seq_count, 0);				// a_{k - 1}
-		std::vector <std::size_t> dst_seq_indices(seq_count, 0);				// a_k
-		std::vector <std::size_t> character_counts;								// Buffer for counting sort
-		std::vector <std::size_t> sorted_divergence(seq_count, 0);				// s_k
-		std::vector <std::size_t> divergence_value_indices(seq_count, 0);		// e_k
-		std::vector <std::size_t> divergence_counts(seq_count, 0);				// t_k
-		std::vector <std::size_t> new_eq_class_positions(seq_count, 0);
-		std::size_t pos{}; // Reverse column index.
-		
-		// Initial order.
-		std::iota(src_seq_indices.begin(), src_seq_indices.end(), 0);
-		sorted_divergence.resize(1);
-		divergence_counts.resize(1);
-		divergence_counts[0] = input_count;
-		// Process.
-		while (reader.fill_buffer())
+		bool is_valid() const { return location.is_valid(); }
+		auto length() const { return location.length(); }
+	};
+	
+	struct scored_interval_location_lb_cmp
+	{
+		bool operator()(scored_interval const &lhs, scored_interval const &rhs) const
 		{
-			auto const *buffer(reader.buffer());
-			for (std::size_t i(0); i < block_size; ++i)
+			return lhs.location.lb < rhs.location.rb;
+		}
+	};
+	
+	struct scored_interval_score_cmp
+	{
+		bool operator()(scored_interval const &lhs, scored_interval const &rhs) const
+		{
+			return lhs.score < rhs.score;
+		}
+	};
+	
+	typedef std::set <scored_interval, scored_interval_location_lb_cmp>	pending_interval_set;
+	typedef std::multiset <scored_interval, scored_interval_score_cmp>	candidate_interval_set;
+	
+	
+	// Read the input segmentation.
+	fg::length_type read_input(std::istream &stream, std::vector <interval> &dst)
+	{
+		cereal::PortableBinaryInputArchive archive(stream);
+		fg::length_type aligned_size{};
+		archive(cereal::make_size_tag(aligned_size));
+		dst.clear();
+		dst.reserve(aligned_size);
+		
+		for (fg::length_type i(0); i < aligned_size; ++i)
+		{
+			auto const lb(aligned_size - i - 1);
+			fg::length_type rb{};
+			archive(fg::length_type(rb));
+			dst.emplace_back(lb, rb + 1); // Store half-open intervals.
+		}
+		
+		return aligned_size;
+	}
+	
+	
+	// Output the segmentation.
+	void output_segmentation(scored_interval const &first_interval)
+	{
+		cereal::PortableBinaryOutputArchive archive(std::cout);
+		
+		// Count the blocks.
+		fg::length_type block_count{};
+		{
+			scored_interval const *current_interval(&first_interval);
+			while (current_interval->is_valid())
 			{
-				auto const access_character([buffer, i, block_size](auto const seq_idx){
-					auto const idx(i * block_size + seq_idx);
-					auto const cc(buffer[idx]);
-					return cc;
-				});
-				
-				// Maintain the reverse column index.
-				++pos;
-				libbio_assert_lte(pos, aligned_size);
-				
-				// Update the sequence order.
-				lb::counting_sort(src_seq_indices, dst_seq_indices, character_counts, access_character);
-				auto const equivalence_class_count(character_counts.size());
-#warning remember to resize character_counts in counting_sort.
-				
-				// Update the pBWT data structures.
-				// We’re processing the columns in reverse order, so the data structures will reflect
-				// longest common prefixes of the suffixes of the text, not vice-versa.
-				// At this point we know that there are character_counts.size() equivalence classes
-				// in the current column. Now we would like to determine, which divergence values
-				// were replaced.
-				std::size_t removed_divergence_values{};
-				std::size_t first_removed_divergence_value_idx{SIZE_MAX};
-				
-				{
-					// Divergenge value indices (e_k)
-					// Position zero is handled separately.
-					new_eq_class_positions.clear();
-					for (std::size_t j(1); j < seq_count; ++j)
-					{
-						auto const seq_idx(dst_seq_indices[j]);
-						if (access_character(j) != access_character(j - 1))
-						{
-							new_eq_class_positions.push_back(j);
-							// If the new count is zero, increase the shift amount.
-							auto const divergence_value_idx(divergence_value_indices[j]);
-							auto &count(divergence_counts[divergence_value_idx]);
-							--count;
-							if (0 == count)
-							{
-								if (SIZE_MAX == first_removed_divergence_value_idx)
-									first_removed_divergence_value_idx = divergence_value_idx;
-								
-								++removed_divergence_values;
-								continue;
-							}
-						}
-					
-						divergence_value_indices[j] -= removed_divergence_values;
-					}
-				}
-				
-				{
-					// Divergence values and counts (s_k and t_k)
-					libbio_assert_eq(sorted_divergence.size(), divergence_counts.size());
-					auto const count{sorted_divergence.size()};
-					if (0 < removed_divergence_values)
-					{
-						// Shift the values and the counts.
-						std::size_t shift_amt{};
-						for (std::size_t j(first_removed_divergence_value_idx); j < count; ++j)
-						{
-							if (0 == divergence_counts[j])
-							{
-								++shift_amt;
-								continue;
-							}
-							
-							libbio_assert_lt(0, shift_amt);
-							sorted_divergence[j - shift_amt] = sorted_divergence[j];
-							divergence_counts[j - shift_amt] = divergence_counts[j];
-						}
-					}
-					
-					// Set the new divergence values.
-					auto const new_count(count - removed_divergence_values + equivalence_class_count);
-					libbio_assert_lte(new_count, 1 + prev_count);
-					sorted_divergence.resize(new_count);
-					divergence_counts.resize(new_count);
-					sorted_divergence.back() = pos;
-					divergence_counts.back() = equivalence_class_count;
-					divergence_value_indices.front() = new_count - 1; // First row.
-					for (auto const j : new_eq_class_positions)
-						divergence_value_indices[j] = new_count - 1;
-				}
-				
-				// Read the segment boundaries. The semi-repeat-free range will be [lb, rb].
-				// Also maintain the memoized values in segment_costs.
-				std::vector <dp_state> segment_costs(aligned_size);
-				std::size_t const lb(aligned_size - pos - 1);
-				std::size_t rb{};
-				iarchive(rb);
-				
-				if (LENGTH_MAX == rb)
-					continue;
-				
-				// [lb, rb] is a semi-repeat-free block. Iterate over the columns where the
-				// number of equivalence classes changes.
-				// FIXME: continue here.
+				++block_count;
+				current_interval = current_interval->next;
 			}
+		}
+		
+		// Output the count.
+		archive(cereal::make_size_tag(block_count));
+		
+		// Output the intervals.
+		{
+			scored_interval const *current_interval(&first_interval);
+			while (current_interval->is_valid())
+			{
+				archive(current_interval->location.lb);
+				archive(current_interval->location.rb);
+				current_interval = current_interval->next;
+			}
+		}
+	}
+	
+	
+	template <bool t_maximize>
+	struct find_optimum
+	{
+		candidate_interval_set::iterator operator()(candidate_interval_set &is) const { return is.begin(); }
+	};
+	
+	template <>
+	struct find_optimum <true>
+	{
+		candidate_interval_set::iterator operator()(candidate_interval_set &is) const { return std::prev(is.end()); }
+	};
+	
+	template <bool t_maximize, typename t_score_fn>
+	void optimize(std::istream &stream, t_score_fn &&score_fn)
+	{
+		std::vector <interval> input_segmentation;
+		auto const aligned_size(read_input(stream, input_segmentation));
+		std::sort(input_segmentation.begin(), input_segmentation.end(), interval_rb_cmp());
+		
+		pending_interval_set pending_intervals;	// Left bounds are distinct.
+		candidate_interval_set candidate_intervals;	// Scores are not distinct.
+		
+		// Add a sentinel so that we don’t need to check for emptiness.
+		candidate_intervals.emplace(fg::LENGTH_MAX, fg::LENGTH_MAX);
+		
+		// For finding min. or max. value.
+		find_optimum <t_maximize> find_optimum;
+		
+		for (auto const &interval : rsv::reverse(input_segmentation))
+		{
+			// Move from pending intervals s.t. interval.rb <= pending.lb.
+			// node_handles ignore the comparator and the key cardinality of the container,
+			// so they can be moved easily. extract() only handles single nodes, not ranges.
+			while (!pending_intervals.empty())
+			{
+				auto const it(std::prev(pending_intervals.end()));
+				if (! (interval.rb <= it->location.lb))
+					break;
+				
+				auto nh(pending_intervals.extract(it));
+				candidate_intervals.insert(std::move(nh));
+			}
+			
+			// Add an entry for the current interval.
+			scored_interval current_interval(interval);
+			
+			// Find the min./max. scoring interval.
+			auto it(find_optimum(candidate_intervals));
+			auto &next_interval(*it);
+			current_interval.next = &next_interval;
+			
+			// Update the score.
+			current_interval.score = score_fn(current_interval, next_interval);
+			
+			// Check if this was the last relevant interval.
+			if (0 == interval.lb)
+			{
+				output_segmentation(current_interval);
+				return;
+			}
+			
+			// Move to pending.
+			auto const res(pending_intervals.emplace(std::move(current_interval)));
+			libbio_assert(res.second);
+		}
+		
+		// For some reason the first block was not found.
+		std::exit(EXIT_FAILURE);
+	}
+	
+	
+	void optimize_segmentation(std::istream &stream, gengetopt_args_info const &args_info)
+	{
+		if (args_info.max_number_of_blocks_given)
+		{
+			optimize <true>(stream, [](scored_interval const &current_interval, scored_interval const &next_interval){
+				return 1 + next_interval.score;
+			});
+		}
+		else if (args_info.min_block_length_given)
+		{
+			optimize <false>(stream, [](scored_interval const &current_interval, scored_interval const &next_interval){
+				return std::max(current_interval.length(), next_interval.score);
+			});
+		}
+		else
+		{
+			std::cerr << "Unknown mode given.\n";
+			std::exit(EXIT_FAILURE);
 		}
 	}
 }
@@ -200,7 +252,17 @@ int main(int argc, char **argv)
 	
 	std::ios_base::sync_with_stdio(false);	// Don't use C style IO after calling cmdline_parser.
 	
-	optimize_segmentation(args_info.sequence_list_arg);
+	if (args_info.segmentation_arg)
+	{
+		lb::file_istream stream;
+		lb::open_file_for_reading(args_info.segmentation_arg, stream);
+		std::cin.tie(nullptr);
+		optimize_segmentation(stream, args_info);
+	}
+	else
+	{
+		optimize_segmentation(std::cin, args_info);
+	}
 	
 	return EXIT_SUCCESS;
 }
