84997b45c715589178fe7ce2065bdcba74c9ca21
Tuukka Norri <tsnorri@iki.fi>
2021-11-13 00:32:54 +0200

    Fix optimization algorithms
    – The earlier implementation did not work correctly for minimizing block length.

--- optimize_segmentation/main.cc
+++ optimize_segmentation/main.cc
@@ -20,8 +20,9 @@ namespace rsv	= ranges::views;
 
 namespace {
 	
-	typedef std::uint64_t score_type;
+	typedef fg::length_type score_type;
 	
+	constexpr static inline score_type	SCORE_MAX{std::numeric_limits <score_type>::max()};
 	
 	// Interval without a score.
 	struct interval
@@ -41,11 +42,19 @@ namespace {
 		fg::length_type length() const { return rb - lb; }
 	};
 	
+	struct interval_lb_cmp
+	{
+		bool operator()(interval const &lhs, interval const &rhs) const
+		{
+			return std::make_tuple(lhs.lb, lhs.rb) < std::make_tuple(rhs.lb, rhs.rb);
+		}
+	};
+	
 	struct interval_rb_cmp
 	{
 		bool operator()(interval const &lhs, interval const &rhs) const
 		{
-			return lhs.rb < rhs.rb;
+			return std::make_tuple(lhs.rb, lhs.lb) < std::make_tuple(rhs.rb, rhs.lb);
 		}
 	};
 	
@@ -69,6 +78,12 @@ namespace {
 		{
 		}
 		
+		scored_interval(fg::length_type const lb_, fg::length_type const rb_, score_type const score_):
+			location(lb_, rb_),
+			score(score_)
+		{
+		}
+		
 		bool is_valid() const { return location.is_valid(); }
 		auto length() const { return location.length(); }
 	};
@@ -81,6 +96,14 @@ namespace {
 		}
 	};
 	
+	struct scored_interval_location_rb_cmp
+	{
+		bool operator()(scored_interval const &lhs, scored_interval const &rhs) const
+		{
+			return lhs.location.rb < rhs.location.rb;
+		}
+	};
+	
 	struct scored_interval_score_cmp
 	{
 		bool operator()(scored_interval const &lhs, scored_interval const &rhs) const
@@ -89,8 +112,63 @@ namespace {
 		}
 	};
 	
-	typedef std::set <scored_interval, scored_interval_location_lb_cmp>	pending_interval_set;
-	typedef std::multiset <scored_interval, scored_interval_score_cmp>	candidate_interval_set;
+	typedef std::set <scored_interval, scored_interval_location_lb_cmp>	interval_by_lb_set;
+	typedef std::multiset <scored_interval, scored_interval_score_cmp>	interval_by_score_set;
+	
+	
+	struct candidate_interval_position
+	{
+		typedef interval_by_score_set::iterator	pointer_type;
+		typedef std::int64_t					position_type;
+		
+		position_type	position{}; // Cache b.c. calculation can be expensive.
+		pointer_type	pointer{};
+		
+		static position_type calculate_position(fg::length_type const j, score_type const s)
+		{
+			// We first rotate the point (j, s) by 45° and then project to the X axis
+			// while also applying some scaling. The resulting very simple tranformation is j - s.
+			libbio_assert_lte(0, j);
+			libbio_assert_lte(0, s);
+			position_type const j_(j);
+			position_type const s_(s);
+			libbio_assert_eq(fg::length_type(j_), j);
+			libbio_assert_eq(fg::length_type(s_), s);
+			position_type const retval(position_type(j) - position_type(s));
+			libbio_assert_lte(retval, j_);
+			return retval;
+		}
+		
+		static position_type calculate_position_lb(scored_interval const &interval_) { return calculate_position(interval_.location.lb, interval_.score); }
+		static position_type calculate_position_rb(scored_interval const &interval_) { return calculate_position(interval_.location.rb, interval_.score); }
+		static position_type calculate_position_lb(pointer_type const pointer_) { return calculate_position_lb(*pointer_); }
+		static position_type calculate_position_rb(pointer_type const pointer_) { return calculate_position_rb(*pointer_); }
+		
+		candidate_interval_position(position_type const position_, pointer_type const pointer_):
+			position(position_),
+			pointer(pointer_)
+		{
+		}
+		
+		// Use left bound by default.
+		explicit candidate_interval_position(pointer_type const pointer_):
+			candidate_interval_position(calculate_position_lb(pointer_), pointer_)
+		{
+		}
+		
+		bool operator<(candidate_interval_position const &other) const { return position < other.position; }
+	};
+	
+	struct candidate_interval_position_cmp
+	{
+		typedef std::true_type is_transparent;
+		
+		bool operator()(candidate_interval_position const &lhs, candidate_interval_position const &rhs) const { return lhs.position < rhs.position; }
+		bool operator()(candidate_interval_position::position_type const lhs, candidate_interval_position const &rhs) const { return lhs < rhs.position; }
+		bool operator()(candidate_interval_position const &lhs, candidate_interval_position::position_type const rhs) const { return lhs.position < rhs; }
+	};
+	
+	typedef std::multiset <candidate_interval_position, candidate_interval_position_cmp> candidate_interval_position_set;
 	
 	
 	// Read the input segmentation.
@@ -100,7 +178,7 @@ namespace {
 		fg::length_type aligned_size{};
 		archive(cereal::make_size_tag(aligned_size));
 		dst.clear();
-		dst.reserve(aligned_size);
+		dst.reserve(1 + aligned_size);
 		
 		for (fg::length_type i(0); i < aligned_size; ++i)
 		{
@@ -140,45 +218,34 @@ namespace {
 		// Output the intervals.
 		{
 			scored_interval const *current_interval(&first_interval);
+			fg::length_type prev_rb{};
 			while (current_interval->is_valid())
 			{
-				archive(current_interval->location.rb);
+				auto const rb(current_interval->location.rb);
+				libbio_assert_lt(prev_rb, rb);
+				archive(rb);
 				current_interval = current_interval->next;
+				prev_rb = rb;
 			}
 		}
 	}
 	
 	
-	template <bool t_maximize>
-	struct find_optimum
+	void max_number_of_blocks(std::istream &stream)
 	{
-		candidate_interval_set::iterator operator()(candidate_interval_set &is) const { return is.begin(); }
-	};
-	
-	template <>
-	struct find_optimum <true>
-	{
-		candidate_interval_set::iterator operator()(candidate_interval_set &is) const { return std::prev(is.end()); }
-	};
-	
-	template <bool t_maximize, typename t_score_fn>
-	void optimize(std::istream &stream, t_score_fn &&score_fn)
-	{
+		// Use an O(n log n) time algorithm to optimize.
 		lb::log_time(std::cerr) << "Loading the input segmentation…\n";
 		std::vector <interval> input_segmentation;
 		auto const aligned_size(read_input(stream, input_segmentation));
 		lb::log_time(std::cerr) << "Sorting…\n";
 		std::sort(input_segmentation.begin(), input_segmentation.end(), interval_rb_cmp());
 		
-		pending_interval_set pending_intervals;	// Left bounds are distinct.
-		candidate_interval_set candidate_intervals;	// Scores are not distinct.
+		interval_by_lb_set pending_intervals;	// Left bounds are distinct.
+		interval_by_score_set candidate_intervals;	// Scores are not distinct.
 		// We actually don’t need the right bounds in candidate_intervals.
 		
 		// Add a sentinel.
 		candidate_intervals.emplace(aligned_size, fg::LENGTH_MAX);
-		
-		// For finding min. or max. value.
-		find_optimum <t_maximize> find_optimum;
 		
 		lb::log_time(std::cerr) << "Optimizing…\n";
 		std::size_t count{};
@@ -196,23 +263,129 @@ namespace {
 				auto const it(std::prev(pending_intervals.end()));
 				if (! (interval.rb <= it->location.lb))
 					break;
-				
+			
 				auto nh(pending_intervals.extract(it));
 				candidate_intervals.insert(std::move(nh));
+			}
+		
+			// Add an entry for the current interval.
+			scored_interval current_interval(interval);
+		
+			// Find the max. scoring interval.
+			{
+				auto it(std::prev(candidate_intervals.end()));
+				auto &next_interval(*it);
+				libbio_assert_lte(current_interval.location.rb, next_interval.location.lb);
+				current_interval.next = &next_interval;
+		
+				// Update the score and the boundaries.
+				current_interval.score = 1 + next_interval.score;
+				current_interval.location.rb = next_interval.location.lb;
+			}
+		
+			// Check if this was the last relevant interval.
+			if (0 == interval.lb)
+			{
+				output_segmentation(current_interval);
+				return;
+			}
+		
+			// Move to pending.
+			{
+				auto const res(pending_intervals.emplace(std::move(current_interval)));
+				libbio_assert(res.second);
+			}
+		}
+		
+		// For some reason the first block was not found.
+		std::exit(EXIT_FAILURE);
+	}
+	
+	
+	void min_block_length(std::istream &stream)
+	{
+		auto const score_fn([](scored_interval const &current, scored_interval const &next){
+			return std::max(next.location.lb - current.location.lb, next.score);
+		});
+		
+		auto const assign_fn([](scored_interval &current, scored_interval const &next, score_type const score){
+			current.score = score;
+			current.next = &next;
+			current.location.rb = next.location.lb;
+		});
+		
+		// Use an O(n log n) time algorithm to optimize.
+		typedef std::multiset <scored_interval, scored_interval_location_lb_cmp> candidate_interval_by_lb_set;
+		
+		// Use an O(n log n) time algorithm to optimize.
+		lb::log_time(std::cerr) << "Loading the input segmentation…\n";
+		std::vector <interval> input_segmentation;
+		auto const aligned_size(read_input(stream, input_segmentation));
+		lb::log_time(std::cerr) << "Sorting…\n";
+		std::sort(input_segmentation.begin(), input_segmentation.end(), interval_rb_cmp());
+		
+		interval_by_lb_set pending_intervals;	// Left bounds are distinct.
+		interval_by_lb_set candidates_by_lb;
+		interval_by_score_set candidates_by_score;
+		candidate_interval_position_set candidate_interval_positions;
+		
+		// Add sentinels.
+		candidates_by_lb.emplace(aligned_size, fg::LENGTH_MAX, 0);
+		candidates_by_score.emplace(aligned_size, fg::LENGTH_MAX, SCORE_MAX); // Make sure this one does not get chosen ever.
+		
+		lb::log_time(std::cerr) << "Optimizing…\n";
+		std::size_t count{};
+		for (auto const &interval : rsv::reverse(input_segmentation))
+		{
+			++count;
+			if (0 == count % 10000000)
+				lb::log_time(std::cerr) << "Interval " << count << '/' << input_segmentation.size() << " (at most)…\n";
+			
+			// Move from pending intervals s.t. interval.rb <= pending.lb.
+			// node_handles ignore the comparator and the key cardinality of the container,
+			// so they can be moved easily. extract() only handles single nodes, not ranges.
+			while (!pending_intervals.empty())
+			{
+				auto const pending_it(std::prev(pending_intervals.end()));
+				if (! (interval.rb <= pending_it->location.lb))
+					break;
+			
+				auto nh(pending_intervals.extract(pending_it));
+				auto const candidate_it(candidates_by_score.insert(std::move(nh)));
+				candidate_interval_positions.emplace(candidate_it); // Uses left bound.
 			}
 			
 			// Add an entry for the current interval.
 			scored_interval current_interval(interval);
 			
-			// Find the min./max. scoring interval.
-			auto it(find_optimum(candidate_intervals));
-			auto &next_interval(*it);
-			libbio_assert_lte(current_interval.location.rb, next_interval.location.lb);
-			current_interval.next = &next_interval;
+			// Since we are iterating in reverse order by right bounds, we can partition
+			// candidate_interval_positions by pos and move the right side to candidates_by_lb.
+			{
+				auto const pos(candidate_interval_position::calculate_position_lb(current_interval));
+				auto it(candidate_interval_positions.lower_bound(pos));
+				auto const it_(it);
+				auto const end(candidate_interval_positions.end());
+				while (it != end)
+				{
+					auto nh(candidates_by_score.extract(it->pointer));
+					auto const res(candidates_by_lb.insert(std::move(nh)));
+					libbio_assert(res.inserted);
+					++it;
+				}
+				candidate_interval_positions.erase(it_, end);
+			}
 			
-			// Update the score and the boundaries.
-			current_interval.score = score_fn(current_interval, next_interval);
-			current_interval.location.rb = next_interval.location.lb;
+			// Find the min. scoring interval.
+			{
+				auto const &next1(*candidates_by_score.begin());
+				auto const &next2(*candidates_by_lb.begin());
+				auto const score1(score_fn(current_interval, next1));
+				auto const score2(score_fn(current_interval, next2));
+				if (score1 < score2)
+					assign_fn(current_interval, next1, score1);
+				else
+					assign_fn(current_interval, next2, score2);
+			}
 			
 			// Check if this was the last relevant interval.
 			if (0 == interval.lb)
@@ -222,29 +395,20 @@ namespace {
 			}
 			
 			// Move to pending.
-			auto const res(pending_intervals.emplace(std::move(current_interval)));
-			libbio_assert(res.second);
+			{
+				auto const res(pending_intervals.emplace(std::move(current_interval)));
+				libbio_assert(res.second);
+			}
 		}
-		
-		// For some reason the first block was not found.
-		std::exit(EXIT_FAILURE);
 	}
 	
 	
 	void optimize_segmentation(std::istream &stream, gengetopt_args_info const &args_info)
 	{
 		if (args_info.max_number_of_blocks_given)
-		{
-			optimize <true>(stream, [](scored_interval const &current_interval, scored_interval const &next_interval){
-				return 1 + next_interval.score;
-			});
-		}
+			max_number_of_blocks(stream);
 		else if (args_info.min_block_length_given)
-		{
-			optimize <false>(stream, [](scored_interval const &current_interval, scored_interval const &next_interval){
-				return std::max(next_interval.location.lb - current_interval.location.lb, next_interval.score);
-			});
-		}
+			min_block_length(stream);
 		else
 		{
 			std::cerr << "Unknown mode given.\n";
